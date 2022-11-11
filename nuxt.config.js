6ab47dbdebee6015cae8aeabae7d8f1ad97fb1cd
Joelle Rublee <jorubelle@icloud.com>
2022-11-11 10:20:18 -0500

    intitial commit

---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+// nuxt.config.js
+
+const strapiBaseUri = process.env.API_URL || "https://blooming-river-40409.herokuapp.com/";
+
+export default {
+
+
+
+  
+   // ENV: https://nuxtjs.org/docs/configuration-glossary/configuration-env
+   env: {
+    strapiBaseUri,
+    stripePublishable: process.env.STRIPE_PUBLISHABLE || 'pk_test_51LkBlcKuOke8qQn9DAgLXJ2YjLcc7c0t9pHPfT2SKwb5wUlyBY0TavCzB8ZXBXjSJLj2BNW3SzJJabftTT5pRWEc00uyGSYNeS'
+  },
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'frontend',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [{ src: 'https://js.stripe.com/v3/' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "uikit/dist/css/uikit.min.css",
+    "uikit/dist/css/uikit.css",
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/uikit.js', ssr: false }
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxt/http', '@nuxtjs/apollo'],
+  // Apollo: https://github.com/nuxt-community/apollo-module#usage
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: `${strapiBaseUri}/graphql`,
+        }
+      }
+    },
+
+// nuxt/http https://http.nuxtjs.org/API/options
+  http: {
+    baseURL: `${strapiBaseUri}/api`,
+    browserBaseURL: `${strapiBaseUri}/api`,
+  },
+  
+
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ store/auth.js
@@ -0,0 +1,31 @@
+
+    // store/auth.js
+    import Cookies from 'js-cookie'
+    
+    export const state = () => ({
+      user: {},
+    })
+    
+    export const mutations = {
+      setUser(state, user) {
+        state.user = user
+        Cookies.set('user', JSON.stringify(user))
+      },
+      logout(state) {
+        state.user = null
+        Cookies.set('user', null)
+      },
+    }
+    
+    export const getters = {
+      user: (state) => {
+        return state.user?.id
+      },
+      username: (state) => {
+        return state.user?.username
+      },
+      token: (state) => {
+        return state.user?.jwt
+      },
+    }
+
